b5ad8b8287d4596d9b71ccd430ff98dca1b3192d
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-03-04 23:35:44 +0530

    functions

--- vpc.tf
+++ vpc.tf
@@ -2,4 +2,5 @@ module "vpc" {
     source = "git::https://github.com/mskumarmadhavarapu/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
+    is_peering_required = true
 }
